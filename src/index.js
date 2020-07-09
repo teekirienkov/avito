3bd16b398b5bd514b8cf07a3c71294f01258c5a3
Timur <teekirienkov@gmail.com>
2020-07-09 21:03:53 +0300

    modal work

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-const modalAdd = document.querySelector('.modal__add'),
-      modalCloseButton = document.querySelector('.modal__close')
-      addAdButton = document.querySelector('.add__ad');
+const modalAdd = document.querySelector('.modal__add'), // все модальные окна (этот класс выпадает на body)
+      addAdButton = document.querySelector('.add__ad'),
+      modalButtonSubmit = document.querySelector('.modal__btn-submit'),
+      modalSubmitForm = document.querySelector('.modal__submit');
+
 
 addAdButton.addEventListener('click', () => {
-  modalAdd.classList.remove('hide')
-})
+  modalAdd.classList.remove('hide');
+  modalButtonSubmit.disabled = true
+});
 
-modalCloseButton.addEventListener('click', (event) => {
-
+modalAdd.addEventListener('click', event => {
   const { target } = event;
 
-  if (target.closest('.modal__close')) {
-    modalAdd.classList.add('hide')
+  if (target.closest('.modal__close') || target === modalAdd) {
+    modalAdd.classList.add('hide');
+    modalSubmitForm.reset();
   }
 })
